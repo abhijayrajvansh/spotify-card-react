6938beda7d42fc67a75626ade9e66f91d2a298d0
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-08-15 21:12:59 +0530

    wagmi: bug fixes & improvements

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Spotify from "@/components/Spotify";
 
 const page = () => {
   return (
-    <div className="flex h-screen items-center">
-      <Spotify />
+    <div className="flex h-screen items-center bg-slate-300">
+      <div className="w-full z-10">
+        <Spotify />
+      </div>
     </div>
   );
 };
