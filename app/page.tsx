3d24095d5366c9f0cadac3322a2ef85aae34d012
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-08-15 21:25:23 +0530

    wagmi: bug fixes & improvements

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,16 @@ import Spotify from "@/components/Spotify";
 
 const page = () => {
   return (
-    <div className="flex h-screen items-center bg-slate-300">
+    <div className="flex h-screen items-center">
       <div className="w-full z-10">
         <Spotify />
+        <div className="flex justify-center">
+          <div className="py-1 px-2 rounded bg-green-200/80 flex justify-center text-white w-fit">
+            <p className="text-green-800">
+              © 2024 Developed by Abhijay Rajvansh
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/Spotify.tsx
+++ components/Spotify.tsx
@@ -12,9 +12,9 @@ const spotifyData = {
 const Spotify = () => {
 
   return (
-      <div className="w-full px-5  rounded-xl justify-center ml-[40%]">
+      <div className="w-full flex rounded-xl justify-center">
         <div
-          className={`relative cursor-pointer rounded-xl font-medium text-lg sm:mt-0 mt-5 sm:ml-[90px] lg:ml-0 sm:w-[650px] lg:max-w-[300px] mb-5 shadow-md transition dark:bg-black/40 bg-black/30 `}
+          className={`relative cursor-pointer rounded-xl font-medium text-lg sm:mt-0 mt-5 sm:w-[650px] lg:max-w-[300px] mb-5 shadow-md transition dark:bg-black/40 bg-black/30 `}
         >
           <Image
             height={500}
